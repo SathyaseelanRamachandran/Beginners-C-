2c79bd408d3788f5f59c49febfb65812357118d9
Raymond <ajayitomisin1728@gmail.com>
2020-05-20 04:51:02 +0100

    Beginners

---
+++ beginners.cpp
@@ -0,0 +1,68 @@
+/*
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"Hello world!!!"<<endl;
+	return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main(){
+	int a;
+	cout<<"Enter first number:";
+	cin>>a;
+	int b;
+	cout<<"Enter second number:";
+	cin>>b;
+	cout<<"The sum of the numbers you entered is:"<<a+b<<endl;
+}
+#include <iostream>
+using namespace std;
+int main(){
+	int a;
+	cout<<"Enter starting point:";
+	cin>>a;
+	int b;
+	cout<<"Enter stop point:";
+	cin>>b;
+	for(int i=a;i<=b;i++){
+	cout<<"The variable i is:"<<i<<endl;
+	}
+	return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main(){
+	int a=0;
+	do{
+		a++;
+		cout<<a<<endl;
+	}
+	while (a<10);
+}
+
+#include <iostream>
+using namespace std;
+int main(){
+	int age;
+	cout<<"Enter your age:";
+	cin>>age;
+	if(age>18){
+	cout<<"You are an adult."<<endl;	
+	}
+	else if(age<=5){
+		cout<<"You are a toddler."<<endl;
+	}
+	else if(age<=12){
+		cout<<"You are a kid."<<endl;
+	}
+	else{
+		cout<<"You are teenager."<<endl;
+	}
+}*/
+
+
+
+
